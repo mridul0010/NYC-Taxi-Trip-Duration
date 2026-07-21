663bb26a3a27261acae613fc08c5c81a036238f4
Mridul <mridullata03@gmail.com>
2026-07-21 11:24:17 +0530

    updated config file

--- src/config.py
+++ src/config.py
@@ -21,6 +21,67 @@ MODELS_DIR = PROJ_ROOT / "models"
 REPORTS_DIR = PROJ_ROOT / "reports"
 FIGURES_DIR = REPORTS_DIR / "figures"
 
+PARAMS_FILE = PROJ_ROOT / "params.yaml"
+
+AVG_EARTH_RADIUS = 6371
+
+# features.py
+TARGET_COLUMN = "trip_duration_min"
+
+HIGH_CARDINALITY_FEATURES = ["pickup_zone", "dropoff_zone", "route_time_density"]
+
+OHE_FEATURES = ["dropoff_state", "pickup_state", "travel_quadrant", "season_name"]
+
+NUMERICAL_COLS = [
+    "vendor_id",
+    "passenger_count",
+    "store_and_fwd_flag",
+    "distance_haversine_km",
+    "distance_manhattan_km",
+    "direction",
+    "hour_of_day",
+    "day_of_week",
+    "month_of_year",
+    "is_rush_hour",
+    "is_interstate_trip",
+    "is_late_night",
+    "is_weekend_night"
+]
+
+LOG_TRANSFORM_FEATURES_BASELINE = [
+    'distance_haversine_km',
+    'distance_manhattan_km'
+]
+
+NUMERICAL_COLS_OSRM = [
+    "vendor_id",
+    "passenger_count",
+    "store_and_fwd_flag",
+    "distance_haversine_km",
+    "distance_manhattan_km",
+    "direction",
+    "hour_of_day",
+    "day_of_week",
+    "month_of_year",
+    "is_rush_hour",
+    "is_interstate_trip",
+    "is_late_night",
+    "is_weekend_night",
+    'total_distance_km',
+    'total_travel_time_min'
+]
+
+LOG_TRANSFORM_FEATURES_OSRM = [
+    'distance_haversine_km',
+    'distance_manhattan_km',
+    'total_distance_km',
+    'total_travel_time_min'
+]
+
+
+
+
+
 # If tqdm is installed, configure loguru with tqdm.write
 # https://github.com/Delgan/loguru/issues/135
 try:
